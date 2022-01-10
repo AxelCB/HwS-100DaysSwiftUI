4e5ddd3e6c01a0e0d8a9a4ee8d493707e4a01b0a
Axel Collard Bovy <axcbovy@gmail.com>
2022-01-09 21:21:11 -0300

    Challenge1: project initialization

---
+++ challenge1/WeConvertUnits/WeConvertUnitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertUnitsApp.swift
+//  WeConvertUnits
+//
+//  Created by Axel Collard Bovy on 09/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertUnitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
