94a2097f2eae5d80cb4d734dc742e6b2c3ed276b
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:41:44 +0100

    Project 18: Technique project learning to use GeometryReader, CoordinateSpace and AlignmentGuide

---
+++ project18/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Axel Collard Bovy on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
