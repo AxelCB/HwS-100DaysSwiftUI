ec8f02cdc63b3027d525f7d6bd36eddcd47c6523
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:40:40 +0100

    Project 17: Initialise new xcode project to be used for Filezilla app

---
+++ project17/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Axel Collard Bovy on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
