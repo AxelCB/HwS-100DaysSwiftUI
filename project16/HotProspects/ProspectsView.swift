187187a97beed4f424b468765b27cecccabe5f02
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:39:45 +0100

    Project 16: Customize ProspectsView to be able to differentiate between different uses of it

--- project16/HotProspects/ProspectsView.swift
+++ project16/HotProspects/ProspectsView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct ProspectsView: View {
+    let filter: FilterType
+
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle(title)
+        }
+    }
+
+    enum FilterType {
+        case none, contacted, uncontacted
     }
 }
 
 struct ProspectsView_Previews: PreviewProvider {
     static var previews: some View {
-        ProspectsView()
+        ProspectsView(filter: .none)
     }
 }
