185bd69285738bcc55275d37d9ec730b3ebb09f6
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 22:24:17 +0100

    Project 5: WorldScramble project initialization

---
+++ project5/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Axel Collard Bovy on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
