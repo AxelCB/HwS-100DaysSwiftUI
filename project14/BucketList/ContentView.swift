041b07817de8b8da7529639a1ab57872f6dfd559
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:37:10 +0100

    Project14: Save current center map location and display as new markers

--- project14/BucketList/ContentView.swift
+++ project14/BucketList/ContentView.swift
@@ -11,11 +11,14 @@ import MapKit
 struct ContentView: View {
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0),
                                                       span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+    @State private var locations = [Location]()
 
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $mapRegion)
-                .ignoresSafeArea()
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+            }
+            .ignoresSafeArea()
             Circle()
                 .fill(.blue)
                 .opacity(0.3)
@@ -25,7 +28,12 @@ struct ContentView: View {
                 HStack {
                     Spacer()
                     Button {
-                        // create a new location
+                        let newLocation = Location(id: UUID(),
+                                                   name: "New Location",
+                                                   description: "",
+                                                   latitude: mapRegion.center.latitude,
+                                                   longitude: mapRegion.center.longitude)
+                        locations.append(newLocation)
                     } label: {
                         Image(systemName: "plus")
                     }
