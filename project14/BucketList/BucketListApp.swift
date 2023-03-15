d3bf10ad749ed78a9acfd31e03de4fdd9882f0cd
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:36:43 +0100

    Project14: Intialisation for BucketList project

---
+++ project14/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Axel Collard Bovy on 18/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
