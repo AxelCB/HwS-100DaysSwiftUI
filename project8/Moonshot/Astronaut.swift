b33367c962618b662ed90704d5aa20e84102a204
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:27:08 +0100

    Project 8: Create astronauts model and load from bundle file

---
+++ project8/Moonshot/Astronaut.swift
@@ -0,0 +1,13 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Axel Collard Bovy on 3/9/22.
+//
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
