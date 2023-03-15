f56decf08de5ea2ae3fb351706196fa4ec881504
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:32:49 +0100

    Project12: Initialise project for CoreData technique project

---
+++ project12/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Axel Collard Bovy on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
