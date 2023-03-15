5072fe65d06b5a05e41e1024109b807201a7c8d6
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:37:23 +0100

    Project14: Make some further improvements to Location model and move some logic away from view and onto the model

--- project14/BucketList/Location.swift
+++ project14/BucketList/Location.swift
@@ -14,4 +14,18 @@ struct Location: Identifiable, Codable, Equatable {
     let description: String
     let latitude: Double
     let longitude: Double
+
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    static let example = Location(id: UUID(),
+                                  name: "Buckingham Palace",
+                                  description: "Where Queen Elizabeth lived with her dorgis.",
+                                  latitude: 51.501,
+                                  longitude: -0.141)
 }
