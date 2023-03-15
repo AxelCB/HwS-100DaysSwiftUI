81a4baab587179b991f1250d1e28c46a9092dbf3
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:30:11 +0100

    Project 11: Initialising project for Bookworm

---
+++ project11/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Axel Collard Bovy on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
