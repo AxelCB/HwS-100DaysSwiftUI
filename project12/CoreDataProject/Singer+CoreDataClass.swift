d3be46ef0b147ff00775a455725cdb35d83a5e29
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:33:47 +0100

    Project 12: Create Singer core data model and class to try out a more dynamic way of filtering

---
+++ project12/CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Axel Collard Bovy on 8/2/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
