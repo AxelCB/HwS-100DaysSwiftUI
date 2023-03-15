694b2a71c0334809c267da88455173e4fd0889d3
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:37:57 +0100

    Project14: Improve FileManager JSON utils extension by making file saving both atomic and protected

--- project14/BucketList/FileManager+DocumentsDirectoryUtils.swift
+++ project14/BucketList/FileManager+DocumentsDirectoryUtils.swift
@@ -11,7 +11,7 @@ extension FileManager {
     func writeJSON<T: Codable>(_ content: T, toFile fileName: String) throws {
         let jsonEncoder = JSONEncoder()
         let data = try jsonEncoder.encode(content)
-        try data.write(to: getFileURLInDocumentsDirectory(for: fileName))
+        try data.write(to: getFileURLInDocumentsDirectory(for: fileName), options: [.atomic, .completeFileProtection])
     }
 
     func readJSON<T: Codable>(fromFile fileName: String) throws -> T {
