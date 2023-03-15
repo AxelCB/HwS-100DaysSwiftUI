1ecffebd653b636bf218d58fc127832d6b6d7955
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:35:47 +0100

    Project13: Wrap PHPickerViewController in SwiftUI (ImagePicker) to be able to pick images

---
+++ project13/Instafilter/ImagePicker.swift
@@ -0,0 +1,22 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Axel Collard Bovy on 13/2/23.
+//
+
+import Foundation
+import SwiftUI
+import PhotosUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+
+        let picker = PHPickerViewController(configuration: config)
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) { }
+}
