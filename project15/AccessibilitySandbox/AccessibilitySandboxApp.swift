9032343d057f517350a1179e796266229feaa1ca
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:38:14 +0100

    Project15: Initialize project to test out accessibility with SwiftUI

---
+++ project15/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Axel Collard Bovy on 26/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
