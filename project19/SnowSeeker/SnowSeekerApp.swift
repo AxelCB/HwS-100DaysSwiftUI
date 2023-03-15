5c3868e36e14b285bfe43548287ef0662bc278aa
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 22:26:04 +0100

    Project 19: Initialise xcode project for Snow Seeker app

---
+++ project19/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Axel Collard Bovy on 15/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
