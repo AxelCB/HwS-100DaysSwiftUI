d36c6de8331061c6ed84b99b3cf6b9b5abf987af
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:38:35 +0100

    Challenge 12: Improve CupcakeCorner(project10) acessibility by skipping Image and ProgresView since they're just decorative

--- project10/CupcakeCorner/CheckoutView.swift
+++ project10/CupcakeCorner/CheckoutView.swift
@@ -22,8 +22,10 @@ struct CheckoutView: View {
                             .scaledToFit()
                 } placeholder: {
                     ProgressView()
+                        .accessibilityHidden(true)
                 }
                 .frame(height: 233)
+                .accessibilityHidden(true)
 
                 Text("Your total is \(orderViewModel.order.cost, format: .currency(code: "USD"))")
                     .font(.title)
