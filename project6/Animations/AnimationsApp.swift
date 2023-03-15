dadc29b7bc1183864316b1bb5b46c9cf677fda48
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 22:24:35 +0100

    Project 6: Create empty project to learn all about animations

---
+++ project6/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Axel Collard Bovy on 22/8/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
