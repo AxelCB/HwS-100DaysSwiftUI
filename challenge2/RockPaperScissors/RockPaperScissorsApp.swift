3e3c25d68031c9fd775d7f14ba58d592d3375208
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 22:23:30 +0100

    Challenge 2: Project creation

---
+++ challenge2/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Axel Collard Bovy on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
