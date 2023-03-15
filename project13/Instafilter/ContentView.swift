8125e538de0f300dd355800ac516dd8bb7cd54ac
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:36:10 +0100

    Project13: Restoring ContentView to initial state

--- project13/Instafilter/ContentView.swift
+++ project13/Instafilter/ContentView.swift
@@ -10,31 +10,9 @@ import CoreImage
 import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingImagePicker = false
 
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-
-            Button("Select Image") {
-               showingImagePicker = true
-            }
-        }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker(image: $inputImage)
-        }
-        .onChange(of: inputImage) { _ in loadImage() }
-    }
-
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-        let imageSaver = ImageSaver()
-        imageSaver.writeToPhotoAlbum(image: inputImage)
+        Text("Hello World!")
     }
 }
 
