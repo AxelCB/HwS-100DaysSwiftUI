e6edb82a2a5e1ed49db07d6a49b1d7a13f3758f5
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:33:54 +0100

    Project 12: Create sample DynamicFilteringContentView to test out FilteredList with Singer model

--- project12/CoreDataProject/ContentView.swift
+++ project12/CoreDataProject/ContentView.swift
@@ -8,6 +8,44 @@
 import SwiftUI
 import CoreData
 
+struct DynamicFilteringContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @State private var lastNameFilter = "A"
+
+    var body: some View {
+        VStack {
+            FilteredList(filterKey: "lastName", filterValue: lastNameFilter) { (singer: Singer) in
+                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+            }
+
+            Button("Add Examples") {
+                let taylor = Singer(context: self.moc)
+                taylor.firstName = "Taylor"
+                taylor.lastName = "Swift"
+
+                let ed = Singer(context: self.moc)
+                ed.firstName = "Ed"
+                ed.lastName = "Sheeran"
+
+                let adele = Singer(context: self.moc)
+                adele.firstName = "Adele"
+                adele.lastName = "Adkins"
+
+                try? self.moc.save()
+            }
+
+            Button("Show A") {
+                self.lastNameFilter = "A"
+            }
+
+            Button("Show S") {
+                self.lastNameFilter = "S"
+            }
+        }
+    }
+}
+
+
 struct ShipContentView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == 'Star Wars'")) var ships: FetchedResults<Ship>
