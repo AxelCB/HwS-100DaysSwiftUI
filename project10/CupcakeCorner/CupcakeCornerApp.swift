f216f23bf37678410919cf03ea15bc90e682a867
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:29:24 +0100

    Project 10: Initialising project for Cupcake Corner

---
+++ project10/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Axel Collard Bovy on 22/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
