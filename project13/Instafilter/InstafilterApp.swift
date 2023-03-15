c3a53e09f28df9ee1627ecaf0af3f2bf6197c002
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:34:50 +0100

    Project13: Initialisation for Instafilter app project

---
+++ project13/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Axel Collard Bovy on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
