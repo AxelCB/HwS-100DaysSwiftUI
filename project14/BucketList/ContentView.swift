0f54738ea409f21440a9116a641d1aff8a4fd1f8
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:36:45 +0100

    Project14: Add basic example of reading and writing to a local file from docuemnts directory

--- project14/BucketList/ContentView.swift
+++ project14/BucketList/ContentView.swift
@@ -14,8 +14,30 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .onTapGesture(perform: loadFile)
         }
         .padding()
+    }
+
+    func loadFile() {
+        let str = "Test Message"
+        let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+
+        do {
+            try str.write(to: url, atomically: true, encoding: .utf8)
+            let input = try String(contentsOf: url)
+            print(input)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+        // just send back the first one, which ought to be the only one
+        return paths[0]
     }
 }
 
