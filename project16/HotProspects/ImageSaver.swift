3be7dc21d12a55b80d219d1e3e31fe355538fab7
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-15 21:40:26 +0100

    Project 16: Add ImageSaver class implemented back in project 13

---
+++ project16/HotProspects/ImageSaver.swift
@@ -0,0 +1,25 @@
+//
+//  ImageSaver.swift
+//  HotProspects
+//
+//  Created by Axel Collard Bovy on 5/3/23.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
