c4f22bd4c1150339fd248504ba695ad39805cff0
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-20 00:30:34 +0100

    Project 19: Allow users to add a resort as favorite and show a heart icon only for favorite resorts in the list

--- project19/SnowSeeker/ContentView.swift
+++ project19/SnowSeeker/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
 
     @State private var searchText = ""
+    @StateObject var favorites = Favorites()
 
     var body: some View {
         NavigationView {
@@ -18,23 +19,31 @@ struct ContentView: View {
                 NavigationLink {
                     ResortView(resort: resort)
                 } label: {
-                    Image(resort.country)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 40, height: 25)
-                        .clipShape(
-                            RoundedRectangle(cornerRadius: 5)
-                        )
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 5)
-                                .stroke(.black, lineWidth: 1)
-                        )
-
-                    VStack(alignment: .leading) {
-                        Text(resort.name)
-                            .font(.headline)
-                        Text("\(resort.runs) runs")
-                            .foregroundColor(.secondary)
+                    HStack{
+                        Image(resort.country)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 25)
+                            .clipShape(
+                                RoundedRectangle(cornerRadius: 5)
+                            )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(.black, lineWidth: 1)
+                            )
+                        
+                        VStack(alignment: .leading) {
+                            Text(resort.name)
+                                .font(.headline)
+                            Text("\(resort.runs) runs")
+                                .foregroundColor(.secondary)
+                        }
+                        if favorites.contains(resort) {
+                            Spacer()
+                            Image(systemName: "heart.fill")
+                                .accessibilityLabel("This is a favorite resort")
+                                .foregroundColor(.red)
+                        }
                     }
                 }
             }
@@ -43,6 +52,7 @@ struct ContentView: View {
 
             WelcomeView()
         }
+        .environmentObject(favorites)
     }
 
     var filteredResorts: [Resort] {
--- project19/SnowSeeker/ResortView.swift
+++ project19/SnowSeeker/ResortView.swift
@@ -13,6 +13,7 @@ struct ResortView: View {
     @Environment(\.horizontalSizeClass) var sizeClass
     @Environment(\.dynamicTypeSize) var typeSize
 
+    @EnvironmentObject var favorites: Favorites
     @State private var selectedFacility: Facility?
     @State private var showingFacility = false
 
@@ -56,6 +57,15 @@ struct ResortView: View {
                 }
                 .padding(.horizontal)
             }
+            Button(favorites.contains(resort) ? "Remove from Favorites" : "Add to Favorites") {
+                if favorites.contains(resort) {
+                    favorites.remove(resort)
+                } else {
+                    favorites.add(resort)
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .padding()
         }
         .navigationTitle("\(resort.name), \(resort.country)")
         .navigationBarTitleDisplayMode(.inline)
@@ -69,5 +79,6 @@ struct ResortView: View {
 struct ResortView_Previews: PreviewProvider {
     static var previews: some View {
         ResortView(resort: Resort.example)
+            .environmentObject(Favorites())
     }
 }
