b0e01c68f72bc5284d9daecee90b7bd278393145
Axel Collard Bovy <axcbovy@gmail.com>
2023-03-28 20:12:41 +0200

    Challenge 16: Implement three different ways of sorting resorts

--- project19/SnowSeeker/ContentView.swift
+++ project19/SnowSeeker/ContentView.swift
@@ -11,38 +11,66 @@ struct ContentView: View {
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
 
     @State private var searchText = ""
+    @State private var sortingOrder: Sorting = .loaded
     @StateObject var favorites = Favorites()
 
     var body: some View {
         NavigationView {
-            List(filteredResorts) { resort in
-                NavigationLink {
-                    ResortView(resort: resort)
-                } label: {
-                    HStack{
-                        Image(resort.country)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 40, height: 25)
-                            .clipShape(
-                                RoundedRectangle(cornerRadius: 5)
-                            )
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 5)
-                                    .stroke(.black, lineWidth: 1)
-                            )
-                        
-                        VStack(alignment: .leading) {
-                            Text(resort.name)
-                                .font(.headline)
-                            Text("\(resort.runs) runs")
-                                .foregroundColor(.secondary)
-                        }
-                        if favorites.contains(resort) {
-                            Spacer()
-                            Image(systemName: "heart.fill")
-                                .accessibilityLabel("This is a favorite resort")
-                                .foregroundColor(.red)
+            Group {
+                HStack {
+                    Button("Default sorting") {
+                        sortingOrder = .loaded
+                    }
+                        .foregroundColor(sortingOrder == .loaded ? .white : .accentColor)
+                        .padding()
+                        .background(sortingOrder == .loaded ? Color.accentColor : Color.white)
+                        .cornerRadius(8)
+                    Button("Sort by resort") {
+                        sortingOrder = .resort
+                    }
+                        .foregroundColor(sortingOrder == .resort ? .white : .accentColor)
+                        .padding()
+                        .background(sortingOrder == .resort ? Color.accentColor : Color.white)
+                        .cornerRadius(8)
+                    Button("Sort by country") {
+                        sortingOrder = .country
+                    }
+                        .foregroundColor(sortingOrder == .country ? .white : .accentColor)
+                        .padding()
+                        .background(sortingOrder == .country ? Color.accentColor : Color.white)
+                        .cornerRadius(8)
+                }
+
+
+                List(orderedResorts) { resort in
+                    NavigationLink {
+                        ResortView(resort: resort)
+                    } label: {
+                        HStack{
+                            Image(resort.country)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 25)
+                                .clipShape(
+                                    RoundedRectangle(cornerRadius: 5)
+                                )
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .stroke(.black, lineWidth: 1)
+                                )
+
+                            VStack(alignment: .leading) {
+                                Text(resort.name)
+                                    .font(.headline)
+                                Text("\(resort.runs) runs")
+                                    .foregroundColor(.secondary)
+                            }
+                            if favorites.contains(resort) {
+                                Spacer()
+                                Image(systemName: "heart.fill")
+                                    .accessibilityLabel("This is a favorite resort")
+                                    .foregroundColor(.red)
+                            }
                         }
                     }
                 }
@@ -61,6 +89,25 @@ struct ContentView: View {
         } else {
             return resorts.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
         }
+    }
+
+    var orderedResorts: [Resort] {
+        filteredResorts.sorted { lhs, rhs in
+            switch sortingOrder {
+            case .loaded:
+                return false
+            case .resort:
+                return lhs.name < rhs.name
+            case .country:
+                return lhs.country < rhs.country
+            }
+        }
+    }
+
+    fileprivate enum Sorting {
+        case loaded
+        case resort
+        case country
     }
 }
 
